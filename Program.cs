0e81d82b754e570959ee26124760a19321b425b0
PomalinaMargo <145930584+PomalinaMargo@users.noreply.github.com>
2023-10-16 10:58:02 +0300

    2
    2

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Program
+
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter an integer:");
+            string num = Console.ReadLine();
+            if (int.TryParse(num, out int number))
+            {
+                Console.WriteLine("You entered a number:" + number);
+            }
+            else
+            {
+                Console.WriteLine("The number you entered is not an integer");
+            }
+        }
+    }
+
+
+
+
+
+
+
+}
